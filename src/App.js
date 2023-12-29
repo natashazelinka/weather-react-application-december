6decc77833cf5b0bcb704939cc9fc292b73ae13d
natashazelinka <natashazelinka@googlemail.com>
2023-12-29 18:42:33 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is created by Natasha Zelinka and is open-sourced
-        <a
-          href="https://github.com/natashazelinka/weather-react-application-december"
-          target="_blank"
-          rel="noreferrer"
-        >
-          on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project is created by Natasha Zelinka and is open-sourced
+          <a
+            href="https://github.com/natashazelinka/weather-react-application-december"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
