c1b53d5a48008549f1dc8bf04fd7c9c48e7eb45e
natashazelinka <natashazelinka@googlemail.com>
2023-12-29 18:23:47 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <a
           href="https://github.com/natashazelinka/weather-react-application-december"
           target="_blank"
+          rel="noreferrer"
         >
           on GitHub
         </a>
